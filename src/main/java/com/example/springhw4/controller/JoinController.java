fbec3b701160645d1024a314acbdf586a7be0bd6
hwangsea <134906042+hwangsea@users.noreply.github.com>
2024-11-09 01:13:08 +0900

    강의 6

---
+++ src/main/java/com/example/springhw4/controller/JoinController.java
@@ -0,0 +1,28 @@
+package com.example.springhw4.controller;
+
+import com.example.springhw4.dto.JoinDTO;
+import com.example.springhw4.service.JoinService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
+public class JoinController {
+
+    private final JoinService joinService;
+
+    public JoinController(JoinService joinService) {
+
+        this.joinService = joinService;
+    }
+
+    @PostMapping("/join")
+    public String joinProcess(JoinDTO joinDTO) {
+
+        System.out.println(joinDTO.getUsername());
+        joinService.joinProcess(joinDTO);
+
+        return "ok";
+    }
+}
